e3ac8c8e16533b9dc75ae0d6d0be1682edeba60f
Ilya <ilia.nikulin@gmail.com>
2018-10-22 18:07:16 +0300

    items: communication

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -2,16 +2,8 @@
     <div>
         <input type="text" class="todo-input" placeholder="What needs to be done" v-model="newTodo" @keyup.enter="addTodo">
         <transition-group name="fade" enter-active-class="animated fadeInUp" leave-active-class="animated fadeOutDown">
-            <div v-for="(todo, index) in todosFiltered" :key="todo.id" class="todo-item">
-                <div class="todo-item-left">
-                    <input type="checkbox" v-model="todo.completed">
-                    <div v-if="!todo.editing" @dblclick="editTodo(todo)" class="todo-item-label" :class="{ completed : todo.completed }">{{ todo.title }}</div>
-                    <input v-else class="todo-item-edit" type="text" v-model="todo.title" @blur="doneEdit(todo)" @keyup.enter="doneEdit(todo)" @keyup.esc="cancelEdit(todo)" v-focus>
-                </div>
-                <div class="remove-item" @click="removeTodo(index)">
-                    &times;
-                </div>
-            </div>
+            <todo-item v-for="todo in todosFiltered" :key="todo.id" :todo="todo" :checkAll="!anyRemaining" @removedTodo="removeTodo" @finishedEdit="finishedEdit">
+            </todo-item>
         </transition-group>
 
         <div class="extra-container">
@@ -37,13 +29,17 @@
 </template>
 
 <script>
+    import TodoItem from './TodoItem'
+
     export default {
         name: 'todo-list',
+        components: {
+            TodoItem,
+        },
         data () {
             return {
                 newTodo: '',
                 idForTodo: 3,
-                beforeEditCache: '',
                 filter: 'all',
                 todos: [
                     {
@@ -83,13 +79,6 @@
                 return this.todos.filter(todo => todo.completed).length > 0
             }
         },
-        directives: {
-            focus: {
-                inserted: function (el) {
-                    el.focus()
-                }
-            }
-        },
         methods: {
             addTodo() {
                 if (this.newTodo.trim().length == 0) {
@@ -100,27 +89,13 @@
                     id: this.idForTodo,
                     title: this.newTodo,
                     completed: false,
-                    editing: false,
                 })
 
                 this.newTodo = ''
                 this.idForTodo++
             },
-            editTodo(todo) {
-                this.beforeEditCache = todo.title
-                todo.editing = true
-            },
-            doneEdit(todo) {
-                if (todo.title.trim() == '') {
-                    todo.title = this.beforeEditCache
-                }
-                todo.editing = false
-            },
-            cancelEdit(todo) {
-                todo.title = this.beforeEditCache
-                todo.editing = false
-            },
-            removeTodo(index) {
+            removeTodo(id) {
+                const index = this.todos.findIndex((item) => item.id == id)
                 this.todos.splice(index, 1)
             },
             checkAllTodos() {
@@ -128,6 +103,10 @@
             },
             clearCompleted() {
                 this.todos = this.todos.filter(todo => !todo.completed)
+            },
+            finishedEdit(data) {
+                const index = this.todos.findIndex((item) => item.id == data.id)
+                this.todos.splice(index, 1, data)
             }
         }
     }
