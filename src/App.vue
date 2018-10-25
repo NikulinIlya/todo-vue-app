e7119d6ea0c9ce6539d63a2b09de4fc1ec774ff1
Ilya <ilia.nikulin@gmail.com>
2018-10-25 16:43:04 +0300

    mk new vue components

--- src/App.vue
+++ src/App.vue
@@ -27,15 +27,16 @@
         max-width: 600px;
         margin: 0 auto;
     }
-    #app {
+
+    /*#app {
         font-family: 'Avenir', Helvetica, Arial, sans-serif;
         -webkit-font-smoothing: antialiased;
         -moz-osx-font-smoothing: grayscale;
-        /*text-align: center;*/
+        !*text-align: center;*!
         color: #2c3e50;
         margin-top: 60px;
         font-size: 23px;
-    }
+    }*/
 
     .logo {
         display: block;
