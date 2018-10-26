6fd5e3731ee945e876daa8e165dce396b1c6daae
Ilya <ilia.nikulin@gmail.com>
2018-10-26 19:44:18 +0300

    auth

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,6 @@
     <div id="app" class="container">
         <img src="./assets/logo.png" class="logo">
         <todo-list></todo-list>
-        <!--<HelloWorld/>-->
     </div>
 </template>
 
@@ -27,16 +26,6 @@
         max-width: 600px;
         margin: 0 auto;
     }
-
-    /*#app {
-        font-family: 'Avenir', Helvetica, Arial, sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        !*text-align: center;*!
-        color: #2c3e50;
-        margin-top: 60px;
-        font-size: 23px;
-    }*/
 
     .logo {
         display: block;
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,9 +1,6 @@
 <template>
     <div>
         <input type="text" class="todo-input" placeholder="What needs to be done" v-model="newTodo" @keyup.enter="addTodo">
-        <div v-if="$store.state.loading" class="lds-ring">
-            <div></div><div></div><div></div><div></div>
-        </div>
         <transition-group name="fade" enter-active-class="animated fadeInUp" leave-active-class="animated fadeOutDown">
             <todo-item v-for="todo in todosFiltered" :key="todo.id" :todo="todo" :checkAll="!anyRemaining">
             </todo-item>
@@ -48,7 +45,6 @@
             }
         },
         created() {
-            this.$store.dispatch('initRealtimeListeners')
             this.$store.dispatch('retrieveTodos')
         },
         computed: {
@@ -77,18 +73,15 @@
 
 <style lang="scss">
     @import url("https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css");
-
     .todo-input {
         width: 100%;
         padding: 10px 18px;
         font-size: 18px;
         margin-bottom: 16px;
-
         &:focus {
             outline: 0;
         }
     }
-
     .todo-item {
         margin-bottom: 12px;
         display: flex;
@@ -96,7 +89,6 @@
         justify-content: space-between;
         animation-duration: 0.3s;
     }
-
     .remove-item {
         cursor: pointer;
         margin-left: 14px;
@@ -104,18 +96,15 @@
             color: black;
         }
     }
-
-    .todo-item-left { // later
+    .todo-item-left {
         display: flex;
         align-items: center;
     }
-
     .todo-item-label {
         padding: 10px;
         border: 1px solid white;
         margin-left: 12px;
     }
-
     .todo-item-edit {
         font-size: 24px;
         color: #2c3e50;
@@ -123,18 +112,15 @@
         width: 100%;
         padding: 10px;
         border: 1px solid #ccc; //override defaults
-        font-family: 'Avenir', Helvetica, Arial, sans-serif;
-
+        font-family: "Avenir", Helvetica, Arial, sans-serif;
         &:focus {
             outline: none;
         }
     }
-
     .completed {
         text-decoration: line-through;
         color: grey;
     }
-
     .extra-container {
         display: flex;
         align-items: center;
@@ -144,72 +130,28 @@
         padding-top: 14px;
         margin-bottom: 14px;
     }
-
     button {
         font-size: 14px;
         background-color: white;
         appearance: none;
         padding: 4px;
-
         &:hover {
             background: lightgreen;
         }
-
         &:focus {
             outline: none;
         }
     }
-
     .active {
         background: lightgreen;
     }
-
     // CSS Transitions
-    .fade-enter-active, .fade-leave-active {
-        transition: opacity .2s;
+    .fade-enter-active,
+    .fade-leave-active {
+        transition: opacity 0.2s;
     }
-
-    .fade-enter, .fade-leave-to {
+    .fade-enter,
+    .fade-leave-to {
         opacity: 0;
     }
-
-    // CSS spinning loader
-    .lds-ring {
-        display: block;
-        position: relative;
-        width: 64px;
-        height: 64px;
-        margin: auto;
-        margin-bottom: 16px;
-    }
-    .lds-ring div {
-        box-sizing: border-box;
-        display: block;
-        position: absolute;
-        width: 51px;
-        height: 51px;
-        margin: 6px;
-        border: 6px solid grey;
-        border-radius: 50%;
-        animation: lds-ring 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
-        border-color: grey transparent transparent transparent;
-    }
-    .lds-ring div:nth-child(1) {
-        animation-delay: -0.45s;
-    }
-    .lds-ring div:nth-child(2) {
-        animation-delay: -0.3s;
-    }
-    .lds-ring div:nth-child(3) {
-        animation-delay: -0.15s;
-    }
-    @keyframes lds-ring {
-        0% {
-            transform: rotate(0deg);
-        }
-        100% {
-            transform: rotate(360deg);
-        }
-    }
-
 </style>
