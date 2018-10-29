bf33ebcb874315bdf5ec3e27329787de8ad057b7
Ilya <ilia.nikulin@gmail.com>
2018-10-29 19:47:52 +0300

    animation

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -80,7 +80,6 @@
 </script>
 
 <style lang="scss">
-    @import url("https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css");
     .todo-input {
         width: 100%;
         padding: 10px 18px;
